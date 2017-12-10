1f1161377d5113b1bd402502cd628f5a37d2fdb0
Maycon de Queiroz Oliveira <mscrat@hotmail.com>
2017-12-10 17:22:22 -0200

    Versão 3
    Multi-Thread

--- Chama.bat
+++ Chama.bat
@@ -2,6 +2,8 @@ gcc Codif.c -o Codif.exe
 if %ERRORLEVEL% == 0 ( GOTO EXEC )
 GOTO OUT
 :EXEC
+ECHO %TIME%
 Codif.exe Teste.tvf Sample01_000.bmp Sample01_001.bmp Sample01_002.bmp Sample01_003.bmp
+ECHO %TIME%
 :OUT
 ECHO %ERRORLEVEL%
--- Codif.c
+++ Codif.c
@@ -1,32 +1,41 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+#include <pthread.h>
 #include <math.h>
-#include <process.h>
-#define VER 1
+#define VER 2
 
 /*
 * Algoritmo de codificacao de imagens bitmap em arquivo de video formato tvf
 */
 
-FILE *IN; //Arquivo de entrada
-FILE *OUT; //Arquivo de saida
+FILE *IN1; //Arquivo de entrada
+FILE *IN2; //Arquivo de entrada para thread 2
+FILE *OUT0; //Arquivo de saida
 
 int L; //4 bytes sendo lidos
 int X; //Auxiliar de L
 int Larg, Altu, LarN; //Largura e altura padrao
+int Sin; //Sinal entre as threads
+int QArg; //Quantidade de argumentos (Para ser global)
+int Fini = 1; //Finalizacao da segunda thread
 //Matrizes: Matriz[Altura][Largura]
 unsigned char N[1080][5760]; //Matriz da imagem atual (sendo processada)
 unsigned char T[1080][1920]; //Matriz da imagem anterior (Ja processada)
+unsigned char N2[1080][5760]; //Matriz da imagem atual (sendo processada)
+unsigned char T2[1080][1920]; //Matriz da imagem anterior (Ja processada)
 unsigned char P[255][3]; //Paleta de cores para acesso direto (sem calculos)
+char **largs; //argv global
 
-//Efetua quantizacao por Floyd–Steinberg (Paleta uniforme)
+//Efetua quantizacao por Floyd–Steinberg (Paleta uniforme) (Primeira Thread)
 void quantiza(){
   int i, j, h;
   unsigned char NR, NG, NB, QP; //Novos R, G e B, Posicao de quantizacao da paleta
   char ER, EG, EB; //Erros de quantizacao
   short B; //Calculo de sinal
-  LarN = (Larg*3)-3;
+  //De baixo para cima, da esquerda para a direita
   for (i = Altu-1; i > 0; i--) {
     //Primeira coluna
     QP = (round(N[i][0]/51.0f) * 42) + (round(N[i][1]/42.5f) * 6) + round(N[i][2]/51.0f);
@@ -104,7 +113,7 @@ void quantiza(){
     N[i-1][j+2] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255;
     T[i][Larg-1] = QP;
   }
-  i = 0;
+  i = 0; //Ultima linha
   for (j = 0; j < LarN; j+=3) {
     QP = (round(N[0][j]/51.0f) * 42) + (round(N[0][j+1]/42.5f) * 6) + round(N[0][j+2]/51.0f);
     ER = N[0][j+2] - P[QP][0]; //Erro
@@ -118,7 +127,110 @@ void quantiza(){
     N[i  ][j+5] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255;
     T[0][j/3] = QP;
   }
+  //Ultimo pixel
   T[0][Larg-1] = (round(N[0][LarN]/51.0f) * 42) + (round(N[0][LarN+1]/42.5f) * 6) + round(N[0][LarN+2]/51.0f);
+}
+
+//Efetua quantizacao por Floyd–Steinberg (Paleta uniforme) (Segunda thread)
+void quantizaT2(){
+  int i, j, h;
+  unsigned char NR, NG, NB, QP; //Novos R, G e B, Posicao de quantizacao da paleta
+  char ER, EG, EB; //Erros de quantizacao
+  short B; //Calculo de sinal
+  //De baixo para cima, da esquerda para a direita
+  for (i = Altu-1; i > 0; i--) {
+    //Primeira coluna
+    QP = (round(N2[i][0]/51.0f) * 42) + (round(N2[i][1]/42.5f) * 6) + round(N2[i][2]/51.0f);
+    ER = N2[i][2] - P[QP][0]; //Erro
+    EG = N2[i][1] - P[QP][1];
+    EB = N2[i][0] - P[QP][2];
+    B = N2[i  ][3] + round(EB * 0.4375);
+    N2[i  ][3] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255 ; //L
+    B = N2[i  ][4] + round(EG * 0.4375);
+    N2[i  ][4] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255 ;
+    B = N2[i  ][5] + round(ER * 0.4375);
+    N2[i  ][5] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255 ;
+    B = N2[i-1][0] + round(EB * 0.3125);
+    N2[i-1][0] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255 ; //S
+    B = N2[i-1][1] + round(EG * 0.3125);
+    N2[i-1][1] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255 ;
+    B = N2[i-1][2] + round(ER * 0.3125);
+    N2[i-1][2] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255 ;
+    B = N2[i-1][3] + round(EB * 0.0625);
+    N2[i-1][3] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255 ; //SE
+    B = N2[i-1][4] + round(EG * 0.0625);
+    N2[i-1][4] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255 ;
+    B = N2[i-1][5] + round(ER * 0.0625);
+    N2[i-1][5] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255 ;
+    T2[i][0] = QP;
+    for (j = 3; j < LarN; j+=3) {
+      //N2[i][j] = B, N2[i][j+1] = G, N2[i][j+2] = R
+      QP = (round(N2[i][j]/51.0f) * 42) + (round(N2[i][j+1]/42.5f) * 6) + round(N2[i][j+2]/51.0f);
+      ER = N2[i][j+2] - P[QP][0]; //Erro
+      EG = N2[i][j+1] - P[QP][1];
+      EB = N2[i][j  ] - P[QP][2];
+      B = N2[i  ][j+3] + round(EB * 0.4375);
+      N2[i  ][j+3] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255; //L
+      B = N2[i  ][j+4] + round(EG * 0.4375);
+      N2[i  ][j+4] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255;
+      B = N2[i  ][j+5] + round(ER * 0.4375);
+      N2[i  ][j+5] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255;
+      B = N2[i-1][j-3] + round(EB * 0.1875);
+      N2[i-1][j-3] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255; //SO
+      B = N2[i-1][j-2] + round(EG * 0.1875);
+      N2[i-1][j-2] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255;
+      B = N2[i-1][j-1] + round(ER * 0.1875);
+      N2[i-1][j-1] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255;
+      B = N2[i-1][j  ] + round(EB * 0.3125);
+      N2[i-1][j  ] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255; //S
+      B = N2[i-1][j+1] + round(EG * 0.3125);
+      N2[i-1][j+1] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255;
+      B = N2[i-1][j+2] + round(ER * 0.3125);
+      N2[i-1][j+2] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255;
+      B = N2[i-1][j+3] + round(EB * 0.0625);
+      N2[i-1][j+3] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255; //SE
+      B = N2[i-1][j+4] + round(EG * 0.0625);
+      N2[i-1][j+4] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255;
+      B = N2[i-1][j+5] + round(ER * 0.0625);
+      N2[i-1][j+5] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255;
+      T2[i][j/3] = QP;
+    }
+    //Ultima coluna
+    j = LarN;
+    QP = (round(N2[i][j]/51.0f) * 42) + (round(N2[i][j+1]/42.5f) * 6) + round(N2[i][j+2]/51.0f);
+    ER = N2[i][LarN+2] - P[QP][0]; //Erro
+    EG = N2[i][LarN+1] - P[QP][1];
+    EB = N2[i][LarN  ] - P[QP][2];
+    B = N2[i-1][j-3] + round(EB * 0.1875);
+    N2[i-1][j-3] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255; //SO
+    B = N2[i-1][j-2] + round(EG * 0.1875);
+    N2[i-1][j-2] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255;
+    B = N2[i-1][j-1] + round(ER * 0.1875);
+    N2[i-1][j-1] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255;
+    B = N2[i-1][j  ] + round(EB * 0.3125);
+    N2[i-1][j  ] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255; //S
+    B = N2[i-1][j+1] + round(EG * 0.3125);
+    N2[i-1][j+1] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255;
+    B = N2[i-1][j+2] + round(ER * 0.3125);
+    N2[i-1][j+2] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255;
+    T2[i][Larg-1] = QP;
+  }
+  i = 0; //Ultima linha
+  for (j = 0; j < LarN; j+=3) {
+    QP = (round(N2[0][j]/51.0f) * 42) + (round(N2[0][j+1]/42.5f) * 6) + round(N2[0][j+2]/51.0f);
+    ER = N2[0][j+2] - P[QP][0]; //Erro
+    EG = N2[0][j+1] - P[QP][1];
+    EB = N2[0][j  ] - P[QP][2];
+    B = N2[i  ][j+3] + round(EB * 0.4375);
+    N2[i  ][j+3] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255; //L
+    B = N2[i  ][j+4] + round(EG * 0.4375);
+    N2[i  ][j+4] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255;
+    B = N2[i  ][j+5] + round(ER * 0.4375);
+    N2[i  ][j+5] = ((B & 255) == B) ? B : (B & 32768) ? 0 : 255;
+    T2[0][j/3] = QP;
+  }
+  //Ultimo pixel
+  T2[0][Larg-1] = (round(N2[0][LarN]/51.0f) * 42) + (round(N2[0][LarN+1]/42.5f) * 6) + round(N2[0][LarN+2]/51.0f);
 }
 
 //Gera a paleta uniforme (Acesso acelerado)
@@ -138,6 +250,43 @@ void gerapaleta(){
   }
 }
 
+//Funcao que a segunda Thread roda
+DWORD WINAPI ThreadFunc(){
+  int k, h, i;
+  for (k = 3; k < QArg; k+=2){
+    IN2 = fopen(largs[k],"rb"); //Leitura Binaria
+    printf("T2. Leitura de %s\n", largs[k]);
+    if (IN2 == NULL){
+      return 2;
+    }
+    fseek(IN2, 10, SEEK_SET);
+    fread(&L, sizeof(L), 1, IN2);
+    if (L != 54){ //Offset bits tem que ser igual a  para imagem true color (nao trabalhamos com paletas)
+      return 5;
+    }
+    fseek(IN2, 54, SEEK_SET); //Pula Cabecalho
+    h = 0;
+    for (i = 0; i < Altu; i++){ //Leitura
+      fread(&N2[i], 1, Larg * 3, IN2);
+      //fwrite(&N[i], 1, Larg * 3, OUT);
+    }
+    //Quantizacao da imagem
+    quantizaT2();
+    while (Sin == 1) {
+      //Espera ativa
+    }
+    for (i = 0; i < Altu; i++) {
+      //T[i][j] = N[i][j];
+      fwrite(&T2[i], 1, Larg, OUT0);
+    }
+    fflush(OUT0);
+    Sin = 1;
+    fclose(IN2);
+  } //Fim para cada arquivo
+  Fini = 0;
+  return 0;
+}
+
 /////////////////////////////////////////////////////////// MAIN
 
 int main(int argc, char *argv[]){
@@ -145,81 +294,101 @@ int main(int argc, char *argv[]){
   if (argc < 3){ //Esperado pelo o menos o programa, arquivo de saida e uma imagem
     return 1;
   }
-  IN = fopen(argv[2],"rb"); //Leitura Binaria
-  if (IN == NULL){
+  IN1 = fopen(argv[2],"rb"); //Leitura Binaria
+  if (IN1 == NULL){
     return 2;
   }
-  OUT = fopen(argv[1],"wb"); //Arquivo de saida
-  if (OUT == NULL){
+  OUT0 = fopen(argv[1],"wb"); //Arquivo de saida
+  if (OUT0 == NULL){
     return 3;
   }
 
   //Leitura do cabecalho
-  fread(&L, sizeof(L), 1, IN);
+  fread(&L, sizeof(L), 1, IN1);
   if ((L & 65535) != 19778){ //BM nao encontrado
     return 4;
   }
-  fseek(IN, 10, SEEK_SET);
-  fread(&L, sizeof(L), 1, IN);
+  fseek(IN1, 10, SEEK_SET);
+  fread(&L, sizeof(L), 1, IN1);
   if (L != 54){ //Offset bits tem que ser igual a para imagem true color (nao trabalhamos com paletas)
     return 5;
   }
 
   //Tamanho (largura e altura) da imagem
-  fseek(IN, 18, SEEK_SET);
-  fread(&Larg, 4, 1, IN);
-  fread(&Altu, 4, 1, IN);
+  fseek(IN1, 18, SEEK_SET);
+  fread(&Larg, 4, 1, IN1);
+  fread(&Altu, 4, 1, IN1);
   if ((Larg & 4095) != Larg || (Altu & 4095) != Altu){
     return 6;
   }
+
+  //Coisas que so acontecem se tudo der certo
   L = (Larg << 20) + (Altu << 8) + (VER << 4); //Escrita do cabecalho do video
-  fwrite(&L, sizeof(int), 1, OUT);
+  fwrite(&L, sizeof(int), 1, OUT0);
   gerapaleta();
+  QArg = argc;
+  largs = argv;
+  Sin = 1;
 
   //Leitura do resto do cabecalho nao importa
-  fseek(IN, 54, SEEK_SET);
+  fseek(IN1, 54, SEEK_SET);
   h = 0;
   for (i = 0; i < Altu; i++){ //Leitura da primeira imagem
-    fread(&N[i], 1, Larg * 3, IN);
+    fread(&N[i], 1, Larg * 3, IN1);
     //fwrite(&N[i], 1, Larg * 3, OUT);
   }
+  LarN = (Larg*3)-3;
   //fflush(OUT);
+
+  //Chamada da thread filha
+  HANDLE thread = CreateThread(NULL, 0, ThreadFunc, NULL, 0, NULL);
 
   //Quantizacao da primeira imagem
   quantiza();
+  while (Sin == 0){
+    //Espera ativa
+  }
   for (i = 0; i < Altu; i++) {
     //T[i][j] = N[i][j];
-    fwrite(&T[i], 1, Larg, OUT);
+    fwrite(&T[i], 1, Larg, OUT0);
   }
-  fflush(OUT);
-  fclose(IN);
+  fflush(OUT0);
+  Sin = 0; //Sinaliza para a outra thread
+  fclose(IN1);
 
-  for (k = 3; k < argc; k++){
-    IN = fopen(argv[k],"rb"); //Leitura Binaria
-    //printf("Leitura de %s\n", argv[k]);
-    if (IN == NULL){
+  for (k = 4; k < argc; k+=2){
+    IN1 = fopen(argv[k],"rb"); //Leitura Binaria
+    printf("T1. Leitura de %s\n", argv[k]);
+    if (IN1 == NULL){
       return 2;
     }
-    fseek(IN, 10, SEEK_SET);
-    fread(&L, sizeof(L), 1, IN);
+    fseek(IN1, 10, SEEK_SET);
+    fread(&L, sizeof(L), 1, IN1);
     if (L != 54){ //Offset bits tem que ser igual a  para imagem true color (nao trabalhamos com paletas)
       return 5;
     }
-    fseek(IN, 54, SEEK_SET); //Pula Cabecalho
+    fseek(IN1, 54, SEEK_SET); //Pula Cabecalho
     h = 0;
     for (i = 0; i < Altu; i++){ //Leitura
-      fread(&N[i], 1, Larg * 3, IN);
+      fread(&N[i], 1, Larg * 3, IN1);
       //fwrite(&N[i], 1, Larg * 3, OUT);
     }
     //Quantizacao da imagem
     quantiza();
+    while (Sin == 0) {
+      //Espera ativa
+    }
     for (i = 0; i < Altu; i++) {
       //T[i][j] = N[i][j];
-      fwrite(&T[i], 1, Larg, OUT);
+      fwrite(&T[i], 1, Larg, OUT0);
     }
-    fflush(OUT);
-    fclose(IN);
+    fflush(OUT0);
+    Sin = 0;
+    fclose(IN1);
   } //Fim para cada arquivo
-  fclose(OUT);
+  while (Fini) {
+    //Espera Ativa
+  }
+  fclose(OUT0);
   return 0;
 }
